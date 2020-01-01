b100634b8970cc0951ab7d7dfe6b455ee7b0e297
Maria-Minerva Vonica <maria.vonica98@gmail.com>
2020-01-01 21:18:48 +0200

    Changed the GUI draft to integrate the table.

--- src/gui/Window.java
+++ src/gui/Window.java
@@ -1,7 +1,7 @@
 package gui;
 
+import java.awt.BorderLayout;
 import java.awt.Button;
-import java.awt.Component;
 import java.awt.Container;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
@@ -9,87 +9,100 @@ import java.awt.GridLayout;
 import java.util.ArrayList;
 
 import javax.swing.BorderFactory;
-import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
 
 import items.Cell;
 
 public class Window {
 	private ArrayList<Cell> cells;
-	
+
 	private ArrayList<JButton> buttons;
-	
+
 	public Window(ArrayList<Cell> cells) {
 		this.cells = cells;
 		this.buttons = new ArrayList<JButton>();
-		
+
 		this.setUp();
 	}
-	
+
 	public void setUp() {
-		 	JFrame frame = new JFrame("Alignment Example");
-		    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		JFrame frame = this.setFrame();
 
-		    Container panel1 = this.createCells();
-		    Container panel2 = layoutComponents("Center", Component.CENTER_ALIGNMENT);
-		    Container panel3 = layoutComponents("Bottom", Component.BOTTOM_ALIGNMENT);
+		Container panel1 = this.createCellsContainer();
+		Container panel2 = this.createParsingContainer();
 
-		    frame.setLayout(new GridLayout(3, 1));
-		    frame.add(panel1);
-		    frame.add(panel2);
-		    frame.add(panel3);
+		frame.add(panel1);
+		frame.add(panel2);
 
-		    frame.setSize(500, 500);
-		    frame.setVisible(true);
+		frame.setSize(500, 500);
+		frame.setVisible(true);
+
 	}
-	
-	public void addButtons(JPanel container, float alignment) {
-		for(Cell cell: this.cells) {
-			JButton newButton = new JButton(Integer.toString(cell.getCoefficient()));
-			container.add(newButton);
-			newButton.setAlignmentY(alignment);
-			buttons.add(newButton);
-		}
+
+	public JFrame setFrame() {
+		JFrame frame = new JFrame("Polynomial Evaluation");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setLayout(new GridLayout(3, 1));
+
+		return frame;
 	}
-	
+
 	protected void makebutton(String name, GridBagLayout gridbag, GridBagConstraints c, JPanel container) {
 		Button button = new Button(name);
 		gridbag.setConstraints(button, c);
 		container.add(button);
-}
-	
-	public Container createCells() {
+	}
+
+	public Container createCellsContainer() {
 		JPanel container = new JPanel();
-	    container.setBorder(BorderFactory.createTitledBorder("Cells"));
-	    
-	    // start creating the grid
+		container.setBorder(BorderFactory.createTitledBorder("Cells"));
+
+		// start creating the grid
 		GridBagLayout gridbag = new GridBagLayout();
-        GridBagConstraints c = new GridBagConstraints();
-	    container.setLayout(gridbag);
-        
-	    c.fill = GridBagConstraints.BOTH;
-	    c.weightx = 1.0;
-	    c.weighty = 1.0;
-	    
-	    for(Cell cell: this.cells) {
+		GridBagConstraints c = new GridBagConstraints();
+		container.setLayout(gridbag);
+
+		// make each cell a box
+		c.fill = GridBagConstraints.BOTH;
+		c.weightx = 1.0;
+		c.weighty = 1.0;
+
+		for (Cell cell : this.cells) {
 			this.makebutton(Integer.toString(cell.getCoefficient()), gridbag, c, container);
 		}
-	    
-	    return container;
+
+		return container;
 	}
-	
-	private Container layoutComponents(String title, float alignment) {
-		// create container basic
-	    JPanel container = new JPanel();
-	    container.setBorder(BorderFactory.createTitledBorder(title));
-	    BoxLayout layout = new BoxLayout(container, BoxLayout.X_AXIS);
-	    container.setLayout(layout);
-	    
-	    // add buttons
-	    this.addButtons(container, alignment);
-	    
-	    return container;
-	  }
+
+	private Container createParsingContainer() {
+		JPanel container = new JPanel();
+		container.setBorder(BorderFactory.createTitledBorder("Parsing"));
+
+		JTable table = this.createTable();
+		JScrollPane scrollPane = new JScrollPane(table);
+		table.setFillsViewportHeight(true);
+
+		container.setLayout(new BorderLayout());
+		container.add(table.getTableHeader(), BorderLayout.PAGE_START);
+		container.add(table, BorderLayout.CENTER);
+
+		return container;
+	}
+
+	private JTable createTable() {
+		String[] columnNames = { "Time", "Cell", "Input", "Output", "X" };
+
+		Object[][] data = { { "Kathy", "Smith", "Snowboarding", new Integer(5), new Boolean(false) },
+				{ "John", "Doe", "Rowing", new Integer(3), new Boolean(true) },
+				{ "Sue", "Black", "Knitting", new Integer(2), new Boolean(false) },
+				{ "Jane", "White", "Speed reading", new Integer(20), new Boolean(true) },
+				{ "Joe", "Brown", "Pool", new Integer(10), new Boolean(false) } };
+
+		JTable table = new JTable(data, columnNames);
+		return table;
+	}
 }
